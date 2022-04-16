502f3e55019afba351c2d1670468fd739a69acb0
unknown <mahmodulshahin665@gmail.com>
2022-04-16 21:37:08 +0600

    call dynamic Api

--- js/countries.js
+++ js/countries.js
@@ -1,3 +1,4 @@
+// this api is from restcountries.com 
 const loadcountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
@@ -11,16 +12,18 @@ const displayCountris = countries => {
     // }
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country)
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        div.appendChild(h3);
-
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+           <h3> ${country.name.common} </h3>
+           <p> ${country.capital} </p>
+           <button onclick = "loadCountryByName('${country.name.common}')"> Details </button>
+        `
         countriesDiv.appendChild(div)
     })
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}
+    `
+    console.log(url)
 }
