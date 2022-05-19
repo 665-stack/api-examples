3983c2a6fd6bddd230b720c4d0e78eae7711f63a
unknown <mahmodulshahin665@gmail.com>
2022-05-19 09:59:42 +0600

    added some style, search box & btn

--- js/api-issues.js
+++ js/api-issues.js
@@ -8,22 +8,37 @@ loadSingleUser()
 const displaySingleUser = user => {
     // console.log(user);
 }
+//=============================================
 // example from mealdb api
+
+// search functionality
+const searchMeal = () => {
+    const searchText = document.getElementById('search-field').value;
+    loadMeals(searchText);
+
+    // clear search input
+    document.getElementById('search-field').value = '';
+}
+
+
+// load api
 const loadMeals = searchText => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
 }
-loadMeals('fish')
+// loadMeals('fish')
 const displayMeals = meals => {
     const container = document.getElementById('meals');
+    container.textContent = '';
     meals.forEach(meal => {
         console.log(meal);
         const div = document.createElement('div');
+        div.classList.add('perMealBox')
         div.innerHTML = `
            <h4>${meal.strMeal}</h4>
-           <button onclick="loadMealDetail('${meal.strMeal}')">Click Me</button>
+           <button class="clickMeBtn" onclick="loadMealDetail('${meal.strMeal}')">Click Me</button>
         `;
         container.appendChild(div);
     });
